ae3c424bfa3a6987cfa28c7b6e56ada6464bf89c
mulonemartin <mulone.martin@gmail.com>
2020-10-08 19:07:11 -0300

    Contract changers stoppable and max mint bpro

---
+++ contracts/changers/MocMakeStoppableChanger.sol
@@ -0,0 +1,33 @@
+pragma solidity 0.5.8;
+import "moc-governance/contracts/Governance/IGovernor.sol";
+import "moc-governance/contracts/Governance/ChangeContract.sol";
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+import "../MoC.sol";
+
+/**
+ * @dev This contract is used to update the configuration of MoC
+ * with MoC --- governance.
+ */
+contract MocMakeStoppableChanger is ChangeContract, Ownable{
+  MoC public moc;
+  bool public isStoppable;
+
+  constructor(MoC _moc, bool _isStoppable) public {
+    moc = _moc;
+    isStoppable = _isStoppable;
+  }
+
+  function execute() external {
+    if (!isStoppable){
+      moc.makeUnstoppable();
+    }
+    else{
+      moc.makeStoppable();
+    }
+  }
+
+  function setStoppable(bool _stoppable) public onlyOwner() {
+    isStoppable = _stoppable;
+  }
+}
+
---
+++ contracts/changers/MocStateMaxMintBProChanger.sol
@@ -0,0 +1,31 @@
+pragma solidity 0.5.8;
+import "../MoCState.sol";
+import "moc-governance/contracts/Governance/ChangeContract.sol";
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+
+/**
+ * @dev This contract is used to update the configuration of MoCState v017
+ * with MoC --- governance.
+ */
+contract MocStateMaxMintBProChanger is ChangeContract, Ownable{
+  MoCState public mocState;
+  uint256 public maxMintBPro;
+
+  constructor(
+    MoCState _mocState,
+    uint256 _maxMintBPro
+  ) public {
+    mocState = _mocState;
+    maxMintBPro = _maxMintBPro;
+  }
+
+  function execute() external {
+    mocState.setMaxMintBPro(maxMintBPro);
+  }
+
+  function setMaxMintBPro(uint256 _maxMintBPro) public onlyOwner() {
+    maxMintBPro = _maxMintBPro;
+  }
+
+
+}
