2c7952c1b50451c5f2855e85a6daab6aff8cbf7f
shanshi <yamilacariati@hotmail.com>
2021-01-22 18:01:47 +0000

    MoC Protection mode test

---
+++ test/process/MoCProtection-test.js
@@ -0,0 +1,65 @@
+const { expectRevert } = require('openzeppelin-test-helpers');
+const BigNumber = require('bignumber.js');
+const testHelperBuilder = require('../mocHelper.js');
+
+let mocHelper;
+
+contract('MoC: Protection mode', function([owner, userAccount, otherAccount, vendorAccount]) {
+  before(async function() {
+    mocHelper = await testHelperBuilder({ owner });
+    ({ toContractBN } = mocHelper);
+    this.moc = mocHelper.moc;
+    this.mocState = mocHelper.mocState;
+    this.mocConnector = mocHelper.mocConnector;
+    this.bprox = mocHelper.bprox;
+    this.governor = mocHelper.governor;
+    this.mockMoCBucketContainerChanger = mocHelper.mockMoCBucketContainerChanger;
+    this.mockMoCVendorsChanger = mocHelper.mockMoCVendorsChanger;
+  });
+
+  beforeEach(async function() {
+    await mocHelper.revertState();
+
+    // Register vendor for test
+    await this.mockMoCVendorsChanger.setVendorsToRegister(
+      await mocHelper.getVendorToRegisterAsArray(vendorAccount, 0)
+    );
+    await this.governor.executeChange(this.mockMoCVendorsChanger.address);
+  });
+
+  describe('GIVEN there are BPros and Docs for a target coverage AND BTC price drops to 3400', function() {
+    beforeEach(async function() {
+      await mocHelper.mintBProAmount(userAccount, 1, vendorAccount);
+      await mocHelper.mintDocAmount(userAccount, 5000, vendorAccount);
+      const liquidationReached = await this.mocState.isLiquidationReached();
+      assert(!liquidationReached, 'Liquidation state should not be reached');
+      await mocHelper.setBitcoinPrice(3400 * mocHelper.MOC_PRECISION);
+
+      const state = await this.mocState.state();
+      mocHelper.assertBig(state, 3, 'State should be AboveCobj');
+    });
+    [
+      { name: 'mintBPro', args: [1, vendorAccount], value: 1, event: 'RiskProMint' },
+      { name: 'redeemFreeDoc', args: [1, vendorAccount], event: 'FreeStableTokenRedeem' }
+    ].forEach(fn => {
+      describe(`WHEN someone executes ${fn.name}`, function() {
+        let tx;
+        it('THEN Moc enters protected mode', async function() {
+          const coverage = await this.mocState.globalCoverage();
+          const _protected = await this.mocState.getProtected();
+          const expected = new BigNumber(coverage).lt(new BigNumber(_protected));
+
+          assert(expected === true, 'coverage should be below protected threshold');
+        });
+        it(`${fn.name} cannot be executed because MoC is in protected mode`, async function() {
+          tx = this.moc[fn.name](...fn.args, {
+            from: otherAccount,
+            value: fn.value ? fn.value * mocHelper.RESERVE_PRECISION : 0
+          });
+
+          await expectRevert(tx, 'Function cannot be called at protection mode');
+        });
+      });
+    });
+  });
+});
