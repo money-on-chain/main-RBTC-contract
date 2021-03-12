444a38523e1e6cc05e83c0e9ce9c813a1ef9fb2a
shanshi <yamilacariati@hotmail.com>
2021-03-12 16:12:55 +0000

    Removed unnecessary lines

--- scripts/deploy/20210308/2_deploy_MoCVendors.js
+++ scripts/deploy/20210308/2_deploy_MoCVendors.js
@@ -1,11 +1,9 @@
 /* eslint-disable no-console */
 const AdminUpgradeabilityProxy = artifacts.require('AdminUpgradeabilityProxy');
-const UpgraderChanger = artifacts.require('./changers/UpgraderChanger.sol');
-const Governor = artifacts.require('moc-governance/contracts/Governance/Governor.sol');
 
 const MoCVendors = artifacts.require('./MoCVendors.sol');
 
-const { getConfig, getNetwork, saveConfig, shouldExecuteChanges } = require('./helper');
+const { getConfig, getNetwork, saveConfig } = require('./helper');
 
 module.exports = async callback => {
   try {
@@ -42,36 +40,6 @@ module.exports = async callback => {
 
     console.log('MoCVendors proxy address: ', proxyMocVendors.address);
     console.log('MoCVendors implementation address: ', mocVendors.address);
-
-    /*
-      COMMENT LINES 16-44 IN CASE OF UPGRADE OF MoCVendors
-      UNCOMMENT LINES 50-74 IN CASE OF UPGRADE OF MoCVendors
-    */
-    // // Deploy contract implementation
-    // console.log('Deploy MoCVendors');
-    // const mocVendors = await MoCVendors.new();
-
-    // // Upgrade contracts with proxy (using the contract address of contract just deployed)
-    // console.log('Upgrade MoCVendors');
-    // const upgradeMocVendors = await UpgraderChanger.new(
-    //   config.proxyAddresses.MoCVendors,
-    //   config.implementationAddresses.UpgradeDelegator,
-    //   mocVendors.address
-    // );
-
-    // // Save implementation address and changer address to config file
-    // config.implementationAddresses.MoCVendors = mocVendors.address;
-    // config.changerAddresses['2_MoCVendors'] = upgradeMocVendors.address;
-    // saveConfig(network, config);
-
-    // if (shouldExecuteChanges(network)) {
-    //   // Execute changes in contracts
-    //   console.log('Execute change - MoCVendors');
-    //   const governor = await Governor.at(config.implementationAddresses.Governor);
-    //   await governor.executeChange(upgradeMocVendors.address);
-    // }
-
-    // console.log('MoCVendors implementation address: ', mocVendors.address);
   } catch (error) {
     callback(error);
   }
