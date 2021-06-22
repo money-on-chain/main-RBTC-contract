02787146859c94e8a1990bff00eaa32bf0d6ab9d
mulonemartin <mulone.martin@gmail.com>
2021-06-22 15:08:42 -0300

    Changer MoCIGovernor

---
+++ contracts/changers/governor/MoCIGovernorChanger.sol
@@ -0,0 +1,25 @@
+pragma solidity 0.5.8;
+import "moc-governance/contracts/Governance/ChangeContract.sol";
+import "moc-governance/contracts/Governance/Governed.sol";
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+/**
+ * @dev This contract is used to update the configuration
+ * with MoC --- governance.
+ */
+contract MoCIGovernorChanger is ChangeContract, Ownable{
+  Governed private mocContract;
+  IGovernor public newGovernor;
+
+  constructor(
+    Governed _mocContract,
+    IGovernor _newGovernor
+  ) public {
+    mocContract = _mocContract;
+    newGovernor = _newGovernor;
+  }
+
+  function execute() external {
+    mocContract.changeIGovernor(newGovernor);
+  }
+
+}
