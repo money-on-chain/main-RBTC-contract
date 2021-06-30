5c1b3b40b6155c6121ba0d8a7ee4989426dd4d11
mulonemartin <mulone.martin@gmail.com>
2021-06-30 14:02:19 -0300

    Change igovernor

--- contracts/changers/governor/MoCIGovernorChanger.sol
+++ contracts/changers/governor/MoCIGovernorChanger.sol
@@ -1,25 +1,25 @@
 pragma solidity 0.5.8;
 import "moc-governance/contracts/Governance/ChangeContract.sol";
-import "moc-governance/contracts/Governance/Governed.sol";
-import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+import "moc-governance/contracts/Upgradeability/UpgradeDelegator.sol";
+
 /**
  * @dev This contract is used to update the configuration
  * with MoC --- governance.
  */
-contract MoCIGovernorChanger is ChangeContract, Ownable{
-  Governed private mocContract;
+contract MoCIGovernorChanger is ChangeContract {
+  UpgradeDelegator public upgradeDelegator;
   IGovernor public newGovernor;
 
   constructor(
-    Governed _mocContract,
+    UpgradeDelegator _upgradeDelegator,
     IGovernor _newGovernor
   ) public {
-    mocContract = _mocContract;
+    upgradeDelegator = _upgradeDelegator;
     newGovernor = _newGovernor;
   }
 
   function execute() external {
-    mocContract.changeIGovernor(newGovernor);
+    upgradeDelegator.changeIGovernor(newGovernor);
   }
 
 }
