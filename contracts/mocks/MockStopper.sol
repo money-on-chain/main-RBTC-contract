a75f4823c84ff86db5e6eec04c834c45662e6822
fruiz08 <fruiz@atixlabs.com>
2023-06-16 15:31:11 -0300

    fix: mockStopper import

--- contracts/mocks/MockStopper.sol
+++ contracts/mocks/MockStopper.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.5.8;
 
-import "../governance/Stopper.sol";
+import "moc-governance/contracts/Stopper/Stopper.sol";
 
 // TODO Think of a better way to
 // force solidity to compile the Stopper so we can require it
