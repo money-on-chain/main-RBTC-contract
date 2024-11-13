3296cfc7916322c1bf051edd3f6489cfaf25f67b
mulonemartin <mulone.martin@gmail.com>
2024-11-13 10:31:17 -0300

    fix: Proposal Flow Addresses changer test

--- scripts/deploy/upgrade_v0.1.17/9_after_vote.js
+++ scripts/deploy/upgrade_v0.1.17/9_after_vote.js
@@ -2,6 +2,8 @@
 const MoCInrate = artifacts.require('./MoCInrate.sol');
 const MoCSettlement = artifacts.require('./MoCSettlement.sol');
 const MoCState = artifacts.require('./MoCState.sol');
+const CommissionSplitterV2 = artifacts.require('./auxiliar/CommissionSplitterV2.sol');
+const CommissionSplitterV3 = artifacts.require('./auxiliar/CommissionSplitterV3.sol');
 const { getConfig, getNetwork } = require('../helper');
 
 module.exports = async callback => {
@@ -35,8 +37,50 @@ module.exports = async callback => {
     contractInfo.MoCSettlement = {}
     contractInfo.MoCSettlement.getBlockSpan = await moCSettlement.getBlockSpan();
 
-    console.log('Contract storage...');
+    const commissionSplitterV2 = await CommissionSplitterV2.at(contractInfo.MoCInrate.commissionsAddress);
+    contractInfo.CommissionSplitterV2 = {}
+    contractInfo.CommissionSplitterV2.outputAddress_1 = await commissionSplitterV2.outputAddress_1();
+    contractInfo.CommissionSplitterV2.outputAddress_2 = await commissionSplitterV2.outputAddress_2();
+    contractInfo.CommissionSplitterV2.outputAddress_3 = await commissionSplitterV2.outputAddress_3();
+    contractInfo.CommissionSplitterV2.outputProportion_1 = await commissionSplitterV2.outputProportion_1();
+    contractInfo.CommissionSplitterV2.outputProportion_2 = await commissionSplitterV2.outputProportion_2();
+    contractInfo.CommissionSplitterV2.outputTokenGovernAddress_1 = await commissionSplitterV2.outputTokenGovernAddress_1();
+    contractInfo.CommissionSplitterV2.outputTokenGovernAddress_2 = await commissionSplitterV2.outputTokenGovernAddress_2();
+    contractInfo.CommissionSplitterV2.outputProportionTokenGovern_1 = await commissionSplitterV2.outputProportionTokenGovern_1();
+    contractInfo.CommissionSplitterV2.tokenGovern = await commissionSplitterV2.tokenGovern();
 
+    const commissionSplitterV3 = await CommissionSplitterV3.at(contractInfo.MoCInrate.getBitProInterestAddress);
+    contractInfo.CommissionSplitterV3 = {}
+    contractInfo.CommissionSplitterV3.outputAddress_1 = await commissionSplitterV3.outputAddress_1();
+    contractInfo.CommissionSplitterV3.outputAddress_2 = await commissionSplitterV3.outputAddress_2();
+    contractInfo.CommissionSplitterV3.outputProportion_1 = await commissionSplitterV3.outputProportion_1();
+
+    console.log('Contract storage...');
+    console.log('');
+    console.log('CommissionSplitterV2');
+    console.log('====================');
+    console.log('');
+    console.log('a. outputAddress_1: ', contractInfo.CommissionSplitterV2.outputAddress_1);
+    console.log('b. outputAddress_2: ', contractInfo.CommissionSplitterV2.outputAddress_2);
+    console.log('c. outputAddress_3: ', contractInfo.CommissionSplitterV2.outputAddress_3);
+    console.log('d. outputProportion_1: ', contractInfo.CommissionSplitterV2.outputProportion_1.toString());
+    console.log('e. outputProportion_2: ', contractInfo.CommissionSplitterV2.outputProportion_2.toString());
+    console.log('g. outputTokenGovernAddress_1: ', contractInfo.CommissionSplitterV2.outputTokenGovernAddress_1);
+    console.log('h. outputTokenGovernAddress_2: ', contractInfo.CommissionSplitterV2.outputTokenGovernAddress_2);
+    console.log('i. outputProportionTokenGovern_1: ', contractInfo.CommissionSplitterV2.outputProportionTokenGovern_1.toString());
+    console.log('j. tokenGovern: ', contractInfo.CommissionSplitterV2.tokenGovern);
+    console.log('');
+    console.log('CommissionSplitterV3');
+    console.log('====================');
+    console.log('');
+    console.log('a. outputAddress_1: ', contractInfo.CommissionSplitterV3.outputAddress_1);
+    console.log('b. outputAddress_2: ', contractInfo.CommissionSplitterV3.outputAddress_2);
+    console.log('c. outputProportion_1: ', contractInfo.CommissionSplitterV3.outputProportion_1.toString());
+    console.log('');
+
+    console.log('Contracts Storage');
+    console.log('=================');
+    console.log('');
 
     console.log('1. MoCInrate getBitProInterestAddress() ->> CommissionSplitterV3: ', contractInfo.MoCInrate.getBitProInterestAddress);
     if (
